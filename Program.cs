a924ea0aef08f5996ce41a01f8da12365a5799b0
jacob <jacob.dekok01@gmail.com>
2022-10-17 15:24:23 -0700

    Fixed Top Comment

--- Program.cs
+++ Program.cs
@@ -1,6 +1,6 @@
 ﻿// Jacob Dekok
 // 10/17/2022
-// Challenge #0 - Mini Challenge #4 Comparing two numbers
+// Git Hub Transfer - Mini Challenge #4 Comparing two numbers
 // This is a console project that will ask the user for two numbers
 // and compare the value of them.
 
